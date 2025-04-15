610a1d6618096f8472e930f6bec86b704387e529
priti24bd <dipapritidas@gmail.com>
2025-04-15 21:52:44 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def collect_data():
+    print("Drone collecting real-time flood data...")
+
+def send_to_cloud():
+    print("Uploading data to cloud storage...")
+
+def alert_community():
+    print("Flood detected! Sending alerts via app and SMS...")
+
+collect_data()
+send_to_cloud()
+alert_community()
